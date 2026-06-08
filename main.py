294c2a697a6b1e55a4fc5057a4d91e88bf4fad3c
aseen9797-lgtm <aseen9797@gmail.com>
2026-06-08 18:46:12 +0530

    added yes or no

--- main.py
+++ main.py
@@ -7,7 +7,10 @@ answer = input("your answer:")
 print("Feedback:")
 for i in keywords:
     if i.lower() in answer.lower():
+        print("yes", i)
         score +=1
+    else:
+        print("no", i)
 print(f"score: {score}/{len(keywords)}") 
 
 
