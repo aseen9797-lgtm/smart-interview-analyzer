9066d039b5926111323f36c8d1c3acfd8939b42a
aseen9797-lgtm <aseen9797@gmail.com>
2026-06-08 19:34:07 +0530

    fixed error

--- main.py
+++ main.py
@@ -1,25 +1,21 @@
-from questions import questions 
-t_score =0
-t_key =0
+from questions import questions
+t_score = 0
+t_key = 0
 for question, keywords in questions.items():
-     print("Question:", question)
-     keywords = questions[question]
-     score =0
-     answer = input("your answer:")
-     print("Feedback:")
-for i in keywords:
-    if i.lower() in answer.lower():
-        print("yes", i)
-        score +=1
-    else:
-        print("no", i)
-print(f"score: {score}/{len(keywords)}") 
-
-t_score += score
-t_key += len(keywords)
-
+    print("\nQuestion:", question)
+    score = 0
+    answer = input("Your answer: ")
+    print("Feedback:")
+    for i in keywords:
+        if i.lower() in answer.lower():
+            print("yes", i)
+            score += 1
+        else:
+            print("no", i)
+    print(f"Score: {score}/{len(keywords)}")
+    t_score += score
+    t_key += len(keywords)
 print("\nInterview Completed!")
-
 print(f"Final Score: {t_score}/{t_key}")
 
 
